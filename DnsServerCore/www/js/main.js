434e55d1e82f7f8d336eac0f077f8838705bc91b
Shreyas Zare <shreyas@technitium.com>
2017-10-01 13:12:14 +0530

    added basic html web app template files

---
+++ DnsServerCore/www/js/main.js
@@ -0,0 +1,5 @@
+﻿
+$(function () {
+    $("#header").html("<div class=\"title\"><a href=\"/\"><img src=\"/img/logo25x25.png\" alt=\"Technitium Logo\" /><span class=\"text\" style=\"color: #ffffff;\">Technitium</span></a></div>");
+    $("#footer").html("<div class=\"content\"><a href=\"https://technitium.com\" target=\"_blank\">Technitium</a> | <a href=\"http://blog.technitium.com\" target=\"_blank\">Blog</a> | <a href=\"https://github.com/TechnitiumSoftware\" target=\"_blank\"><i class=\"fa fa-github\"></i>&nbsp;GitHub</a> | <a href=\"https://technitium.com/aboutus.html\" target=\"_blank\">About</a></div>");
+});
