889747157bdc7798e58919eafa8ca4cf1a92aac7
Shreyas Zare <shreyas@technitium.com>
2017-12-29 22:12:29 +0530

    DnsServerApp: updated project to use .net framework 4.6.1 for using .net standard 2.0 code.

--- DnsServerApp/Properties/AssemblyInfo.cs
+++ DnsServerApp/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("143d7aa6-d01a-4a13-90f5-b650bddc8410")]
+[assembly: Guid("c166a19b-b533-45f8-813f-861dab6d1865")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.0.1")]
+[assembly: AssemblyFileVersion("1.2.0.1")]
