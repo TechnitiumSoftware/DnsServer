4a113c802cea3593e02daf0ee4ad9bb0a4df737d
Shreyas Zare <shreyas@technitium.com>
2020-06-06 16:29:51 +0530

    minor refactoring change.

--- DnsServerCore/Dns/ResolverPrefetchDnsCache.cs
+++ DnsServerCore/Dns/ResolverPrefetchDnsCache.cs
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-using DnsServerCore.Dns.Zones;
+using DnsServerCore.Dns.ZoneManagers;
 using TechnitiumLibrary.Net.Dns;
 
 namespace DnsServerCore.Dns
