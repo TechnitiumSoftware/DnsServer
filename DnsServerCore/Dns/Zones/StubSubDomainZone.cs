a43a054169565bb99a70e67cab48234dc18a0b67
Shreyas Zare <shreyas@technitium.com>
2020-06-06 16:22:02 +0530

    added StubSubDomainZone

---
+++ DnsServerCore/Dns/Zones/StubSubDomainZone.cs
@@ -0,0 +1,32 @@
+﻿/*
+Technitium DNS Server
+Copyright (C) 2020  Shreyas Zare (shreyas@technitium.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+namespace DnsServerCore.Dns.Zones
+{
+    public class StubSubDomainZone : SubDomainZone
+    {
+        #region constructor
+
+        public StubSubDomainZone(string name)
+            : base(name)
+        { }
+
+        #endregion
+    }
+}
