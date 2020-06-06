8154051840fec782bb413abd01918fe582f1ea01
Shreyas Zare <shreyas@technitium.com>
2020-06-06 16:21:40 +0530

    added SecondarySubDomainZone.

---
+++ DnsServerCore/Dns/Zones/SecondarySubDomainZone.cs
@@ -0,0 +1,60 @@
+﻿/*
+Technitium DNS Server
+Copyright (C) 2020  Shreyas Zare (shreyas@technitium.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+using System;
+using System.Collections.Generic;
+using TechnitiumLibrary.Net.Dns;
+
+namespace DnsServerCore.Dns.Zones
+{
+    public class SecondarySubDomainZone : SubDomainZone
+    {
+        #region constructor
+
+        public SecondarySubDomainZone(string name)
+            : base(name)
+        { }
+
+        #endregion
+
+        #region public
+
+        public override void SetRecords(DnsResourceRecordType type, IReadOnlyList<DnsResourceRecord> records)
+        {
+            throw new InvalidOperationException("Cannot set records in secondary zone.");
+        }
+
+        public override void AddRecord(DnsResourceRecord record)
+        {
+            throw new InvalidOperationException("Cannot add record in secondary zone.");
+        }
+
+        public override bool DeleteRecord(DnsResourceRecordType type, DnsResourceRecordData record)
+        {
+            throw new InvalidOperationException("Cannot delete record in secondary zone.");
+        }
+
+        public override bool DeleteRecords(DnsResourceRecordType type)
+        {
+            throw new InvalidOperationException("Cannot delete records in secondary zone.");
+        }
+
+        #endregion
+    }
+}
