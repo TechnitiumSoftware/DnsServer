6c25e9498e8a2cc086e650482782d995a5307d72
Shreyas Zare <shreyas@technitium.com>
2020-06-27 11:51:43 +0530

    ForwarderZone: added SetRecords() and AddRecord() methods with validation checks.

--- DnsServerCore/Dns/Zones/ForwarderZone.cs
+++ DnsServerCore/Dns/Zones/ForwarderZone.cs
@@ -17,6 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
+using System;
+using System.Collections.Generic;
 using TechnitiumLibrary.Net.Dns;
 using TechnitiumLibrary.Net.Dns.ResourceRecords;
 
@@ -38,6 +40,42 @@ namespace DnsServerCore.Dns.Zones
             DnsResourceRecord fwdRecord = new DnsResourceRecord(name, DnsResourceRecordType.FWD, DnsClass.IN, 0, new DnsForwarderRecord(forwarderProtocol, forwarder));
 
             _entries[DnsResourceRecordType.FWD] = new DnsResourceRecord[] { fwdRecord };
+        }
+
+        #endregion
+
+        #region public
+
+        public override void SetRecords(DnsResourceRecordType type, IReadOnlyList<DnsResourceRecord> records)
+        {
+            switch (type)
+            {
+                case DnsResourceRecordType.CNAME:
+                    throw new InvalidOperationException("Cannot set CNAME record to zone root.");
+
+                case DnsResourceRecordType.NS:
+                    throw new InvalidOperationException("Cannot set NS record to forwarder zone root.");
+
+                case DnsResourceRecordType.SOA:
+                    throw new InvalidOperationException("Cannot set SOA record to forwarder zone root.");
+
+                default:
+                    base.SetRecords(type, records);
+                    break;
+            }
+        }
+
+        public override void AddRecord(DnsResourceRecord record)
+        {
+            switch (record.Type)
+            {
+                case DnsResourceRecordType.NS:
+                    throw new InvalidOperationException("Cannot add NS record at forwarder zone root.");
+
+                default:
+                    base.AddRecord(record);
+                    break;
+            }
         }
 
         #endregion
