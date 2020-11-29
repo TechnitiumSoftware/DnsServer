fe3b37fd1b3e24e500d6e14b916306e6d94ab511
Shreyas Zare <shreyas@technitium.com>
2020-11-29 18:14:33 +0530

    webapp: fixed html encoding issue for quotes.

--- DnsServerCore/www/js/common.js
+++ DnsServerCore/www/js/common.js
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 function htmlEncode(value) {
-    return $('<div/>').text(value).html();
+    return $('<div/>').text(value).html().replace(/"/g, "&quot;");
 }
 
 function htmlDecode(value) {
