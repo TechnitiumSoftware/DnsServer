ec1854a95411241043407a9566a246be69d7cb1a
Shreyas Zare <shreyas@technitium.com>
2021-01-02 18:21:44 +0530

    uninstall.sh: removed supervisor support.

--- DnsServerApp/uninstall.sh
+++ DnsServerApp/uninstall.sh
@@ -21,16 +21,13 @@ then
 		rm /etc/resolv.conf >/dev/null 2>&1
 		echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 		echo "nameserver 1.1.1.1" >> /etc/resolv.conf
-	else
-		rm /etc/supervisor/conf.d/dns.conf >/dev/null 2>&1
-		service supervisor restart >/dev/null 2>&1
 	fi
 
 	rm -rf $dnsDir >/dev/null 2>&1
 
 	if [ -d $dotnetDir ]
 	then
-		echo "Uninstalling .NET Core Runtime..."
+		echo "Uninstalling .NET 5 Runtime..."
 		rm /usr/bin/dotnet >/dev/null 2>&1
 		rm -rf $dotnetDir >/dev/null 2>&1
 	fi
