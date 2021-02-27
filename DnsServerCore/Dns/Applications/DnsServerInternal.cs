b8e6f971e58051a6065bcf6693f0c395dcd25b96
Shreyas Zare <shreyas@technitium.com>
2021-02-27 19:42:30 +0530

    added DnsServerInternal

---
+++ DnsServerCore/Dns/Applications/DnsServerInternal.cs
@@ -0,0 +1,65 @@
+﻿/*
+Technitium DNS Server
+Copyright (C) 2021  Shreyas Zare (shreyas@technitium.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+using System.Threading.Tasks;
+using TechnitiumLibrary.Net.Dns;
+using TechnitiumLibrary.Net.Proxy;
+
+namespace DnsServerCore.Dns.Applications
+{
+    class DnsServerInternal : IDnsServer
+    {
+        #region variables
+
+        readonly DnsServer _dnsServer;
+        readonly string _packagePath;
+
+        #endregion
+
+        #region constructor
+
+        public DnsServerInternal(DnsServer dnsServer, string packagePath)
+        {
+            _dnsServer = dnsServer;
+            _packagePath = packagePath;
+        }
+
+        #endregion
+
+        public Task<DnsDatagram> DirectQueryAsync(DnsQuestionRecord question, int timeout = 2000)
+        {
+            return _dnsServer.DirectQueryAsync(question, timeout);
+        }
+
+        public string ServerDomain
+        { get { return _dnsServer.ServerDomain; } }
+
+        public string PackageFolder
+        { get { return _packagePath; } }
+
+        public IDnsCache DnsCache
+        { get { return _dnsServer.DnsCache; } }
+
+        public NetProxy Proxy
+        { get { return _dnsServer.Proxy; } }
+
+        public bool PreferIPv6
+        { get { return _dnsServer.PreferIPv6; } }
+    }
+}
