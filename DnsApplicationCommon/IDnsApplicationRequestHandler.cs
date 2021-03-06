d432d847e031224269dd598fad2a0e999c94ed4a
Shreyas Zare <shreyas@technitium.com>
2021-03-06 16:27:16 +0530

    added API documentation.

--- DnsApplicationCommon/IDnsApplicationRequestHandler.cs
+++ DnsApplicationCommon/IDnsApplicationRequestHandler.cs
@@ -26,12 +26,34 @@ namespace DnsApplicationCommon
 {
     public interface IDnsApplicationRequestHandler : IDisposable
     {
+        /// <summary>
+        /// Allows initializing the DNS application with a config. This function is also called when the config is updated to allow reloading.
+        /// </summary>
+        /// <param name="dnsServer">The DNS server interface object that allows access to DNS server properties.</param>
+        /// <param name="config">The DNS application config stored in the <c>dnsApp.config</c> file.</param>
         Task InitializeAsync(IDnsServer dnsServer, string config);
 
+        /// <summary>
+        /// This method is called by the DNS Server to process the DNS application request when an APP record in an Application zone is hit.
+        /// </summary>
+        /// <param name="request">The DNS request object to be processed.</param>
+        /// <param name="remoteEP">The end point (IP address and port) of the client making the request.</param>
+        /// <param name="zoneName">The name of the application zone that the APP record belongs to.</param>
+        /// <param name="appRecordTtl">The TTL value set in the APP record.</param>
+        /// <param name="appRecordData">The record data in the APP record as required for processing the request.</param>
+        /// <param name="isRecursionAllowed">Tells if the DNS server is configured to allow recursion for the client making this request.</param>
+        /// <param name="dnsServer">The DNS server object that allows access to the DNS server properties.</param>
+        /// <returns>The DNS response for the DNS request or <c>null</c> to send no answer response with an SOA authority.</returns>
         Task<DnsDatagram> ProcessRequestAsync(DnsDatagram request, IPEndPoint remoteEP, string zoneName, uint appRecordTtl, string appRecordData, bool isRecursionAllowed, IDnsServer dnsServer);
 
+        /// <summary>
+        /// The description about this app to be shown in the Apps section of the DNS web console.
+        /// </summary>
         string Description { get; }
 
+        /// <summary>
+        /// A template of the record data format that is required by this app. This template is populated in the UI to allow the user to edit in the expected values. The format could be JSON or any other custom text based format which the app is programmed to parse. This property is optional and can return <c>null</c> if no APP record data is required by the app.
+        /// </summary>
         string ApplicationRecordDataTemplate { get; }
     }
 }
--- DnsApplicationCommon/IDnsServer.cs
+++ DnsApplicationCommon/IDnsServer.cs
@@ -26,20 +26,49 @@ namespace DnsApplicationCommon
 {
     public interface IDnsServer
     {
+        /// <summary>
+        /// Allows querying the DNS server directly. This call supports recursion even if its not enabled in the DNS server configuration. The request and its response are not counted in any stats or logged..
+        /// </summary>
+        /// <param name="question">The question record containing the details to query.</param>
+        /// <param name="timeout">The time the request should wait async for an response.</param>
+        /// <returns>The DNS response for the DNS query or <c>null</c> if the request timed out.</returns>
         Task<DnsDatagram> DirectQueryAsync(DnsQuestionRecord question, int timeout = 2000);
 
+        /// <summary>
+        /// Writes a log entry to the DNS server log file.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
         void WriteLog(string message);
 
+        /// <summary>
+        /// Writes a log entry to the DNS server log file.
+        /// </summary>
+        /// <param name="ex">The exception to log.</param>
         void WriteLog(Exception ex);
 
+        /// <summary>
+        /// The primary domain name used by this DNS Server to identify itself.
+        /// </summary>
         string ServerDomain { get; }
 
+        /// <summary>
+        /// The folder where this application is saved on the disk. Can be used to create temp files, read/write files, etc. for this application.
+        /// </summary>
         string ApplicationFolder { get; }
 
+        /// <summary>
+        /// The DNS cache object which provides direct access to the DNS server cache.
+        /// </summary>
         IDnsCache DnsCache { get; }
 
+        /// <summary>
+        /// The proxy server setting on the DNS server to be used when required to make any outbound network connection.
+        /// </summary>
         NetProxy Proxy { get; }
 
+        /// <summary>
+        /// Tells if the DNS server prefers using IPv6 as per the settings.
+        /// </summary>
         bool PreferIPv6 { get; }
     }
 }
