ebc8150ec8f96177d308d07a512d3fd35f54e368
Shreyas Zare <shreyas@technitium.com>
2021-03-07 15:47:55 +0530

    minor changes in about form

--- DnsServerSystemTrayApp/frmAbout.Designer.cs
+++ DnsServerSystemTrayApp/frmAbout.Designer.cs
@@ -51,8 +51,9 @@
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(56, 261);
+            this.panel1.Size = new System.Drawing.Size(65, 301);
             this.panel1.TabIndex = 21;
             // 
             // pictureBox1
@@ -60,10 +61,11 @@
             this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 205);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 243);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.pictureBox1.Size = new System.Drawing.Size(56, 56);
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.pictureBox1.Size = new System.Drawing.Size(65, 58);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
@@ -71,9 +73,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Arial", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(57)))), ((int)(((byte)(69)))));
-            this.label2.Location = new System.Drawing.Point(75, 24);
+            this.label2.Location = new System.Drawing.Point(88, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(456, 46);
             this.label2.TabIndex = 24;
@@ -82,11 +85,12 @@
             // label4
             // 
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label4.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(57)))), ((int)(((byte)(69)))));
-            this.label4.Location = new System.Drawing.Point(62, 193);
+            this.label4.Location = new System.Drawing.Point(72, 223);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(436, 44);
+            this.label4.Size = new System.Drawing.Size(509, 51);
             this.label4.TabIndex = 33;
             this.label4.Text = resources.GetString("label4.Text");
             // 
@@ -94,9 +98,10 @@
             // 
             this.lnkTerms.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.lnkTerms.AutoSize = true;
-            this.lnkTerms.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkTerms.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lnkTerms.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
-            this.lnkTerms.Location = new System.Drawing.Point(62, 237);
+            this.lnkTerms.Location = new System.Drawing.Point(72, 273);
+            this.lnkTerms.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lnkTerms.Name = "lnkTerms";
             this.lnkTerms.Size = new System.Drawing.Size(116, 15);
             this.lnkTerms.TabIndex = 32;
@@ -109,9 +114,10 @@
             // 
             this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnClose.Location = new System.Drawing.Point(565, 229);
+            this.btnClose.Location = new System.Drawing.Point(659, 264);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.Size = new System.Drawing.Size(88, 27);
             this.btnClose.TabIndex = 31;
             this.btnClose.Text = "&Close";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -119,22 +125,24 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(57)))), ((int)(((byte)(69)))));
-            this.label3.Location = new System.Drawing.Point(476, 144);
+            this.label3.Location = new System.Drawing.Point(555, 166);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.Size = new System.Drawing.Size(58, 16);
             this.label3.TabIndex = 37;
             this.label3.Text = "Website";
             // 
             // lnkWebsite
             // 
             this.lnkWebsite.AutoSize = true;
-            this.lnkWebsite.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkWebsite.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lnkWebsite.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
-            this.lnkWebsite.Location = new System.Drawing.Point(476, 160);
+            this.lnkWebsite.Location = new System.Drawing.Point(555, 185);
+            this.lnkWebsite.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lnkWebsite.Name = "lnkWebsite";
-            this.lnkWebsite.Size = new System.Drawing.Size(129, 16);
+            this.lnkWebsite.Size = new System.Drawing.Size(128, 16);
             this.lnkWebsite.TabIndex = 36;
             this.lnkWebsite.TabStop = true;
             this.lnkWebsite.Text = "technitium.com/dns";
@@ -144,22 +152,24 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(57)))), ((int)(((byte)(69)))));
-            this.label1.Location = new System.Drawing.Point(476, 99);
+            this.label1.Location = new System.Drawing.Point(555, 114);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.Size = new System.Drawing.Size(56, 16);
             this.label1.TabIndex = 35;
             this.label1.Text = "Contact";
             // 
             // lnkContactEmail
             // 
             this.lnkContactEmail.AutoSize = true;
-            this.lnkContactEmail.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkContactEmail.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lnkContactEmail.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
-            this.lnkContactEmail.Location = new System.Drawing.Point(476, 115);
+            this.lnkContactEmail.Location = new System.Drawing.Point(555, 133);
+            this.lnkContactEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lnkContactEmail.Name = "lnkContactEmail";
-            this.lnkContactEmail.Size = new System.Drawing.Size(164, 16);
+            this.lnkContactEmail.Size = new System.Drawing.Size(163, 16);
             this.lnkContactEmail.TabIndex = 34;
             this.lnkContactEmail.TabStop = true;
             this.lnkContactEmail.Text = "support@technitium.com";
@@ -169,9 +179,10 @@
             // labVersion
             // 
             this.labVersion.AutoSize = true;
-            this.labVersion.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labVersion.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.labVersion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(57)))), ((int)(((byte)(69)))));
-            this.labVersion.Location = new System.Drawing.Point(86, 132);
+            this.labVersion.Location = new System.Drawing.Point(100, 152);
+            this.labVersion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labVersion.Name = "labVersion";
             this.labVersion.Size = new System.Drawing.Size(102, 18);
             this.labVersion.TabIndex = 38;
@@ -180,9 +191,10 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(57)))), ((int)(((byte)(69)))));
-            this.label5.Location = new System.Drawing.Point(84, 103);
+            this.label5.Location = new System.Drawing.Point(98, 119);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(206, 29);
             this.label5.TabIndex = 39;
@@ -191,11 +203,11 @@
             // frmAbout
             // 
             this.AcceptButton = this.btnClose;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
             this.CancelButton = this.btnClose;
-            this.ClientSize = new System.Drawing.Size(652, 261);
+            this.ClientSize = new System.Drawing.Size(761, 301);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.labVersion);
             this.Controls.Add(this.label3);
@@ -209,6 +221,7 @@
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmAbout";
