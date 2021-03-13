c83c003976b32299dafac7b7054c89f2cdd2bf92
Shreyas Zare <shreyas@technitium.com>
2021-03-13 19:40:58 +0530

    frmAbout: fixed form icon resource issue causing app to crash.

--- DnsServerSystemTrayApp/frmAbout.Designer.cs
+++ DnsServerSystemTrayApp/frmAbout.Designer.cs
@@ -53,19 +53,19 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(65, 301);
+            this.panel1.Size = new System.Drawing.Size(58, 301);
             this.panel1.TabIndex = 21;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Image = global::DnsServerSystemTrayApp.Properties.Resources.logo;
             this.pictureBox1.Location = new System.Drawing.Point(0, 243);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Padding = new System.Windows.Forms.Padding(5, 5, 5, 5);
-            this.pictureBox1.Size = new System.Drawing.Size(65, 58);
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.pictureBox1.Size = new System.Drawing.Size(58, 58);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
