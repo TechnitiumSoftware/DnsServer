cfbf7b689edac0aaa2b7d406ea5591a1fa604eac
Shreyas Zare <shreyas@technitium.com>
2021-04-10 13:30:15 +0530

    added SimpleCNAME for split horizon.

---
+++ Apps/SplitHorizonApp/SimpleCNAME.cs
@@ -0,0 +1,98 @@
+﻿/*
+Technitium DNS Server
+Copyright (C) 2021  Shreyas Zare (shreyas@technitium.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+using DnsApplicationCommon;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using TechnitiumLibrary.Net;
+using TechnitiumLibrary.Net.Dns;
+using TechnitiumLibrary.Net.Dns.ResourceRecords;
+
+namespace SplitHorizon
+{
+    public class SimpleCNAME : IDnsApplicationRequestHandler
+    {
+        #region IDisposable
+
+        public void Dispose()
+        {
+            //do nothing
+        }
+
+        #endregion
+
+        #region public
+
+        public Task InitializeAsync(IDnsServer dnsServer, string config)
+        {
+            //no config needed
+            return Task.CompletedTask;
+        }
+
+        public Task<DnsDatagram> ProcessRequestAsync(DnsDatagram request, IPEndPoint remoteEP, string zoneName, uint appRecordTtl, string appRecordData, bool isRecursionAllowed, IDnsServer dnsServer)
+        {
+            dynamic jsonAppRecordData = JsonConvert.DeserializeObject(appRecordData);
+            dynamic jsonCname;
+
+            if (NetUtilities.IsPrivateIP(remoteEP.Address))
+                jsonCname = jsonAppRecordData.@private;
+            else
+                jsonCname = jsonAppRecordData.@public;
+
+            if (jsonCname == null)
+                return Task.FromResult<DnsDatagram>(null);
+
+            string cname = jsonCname.Value;
+            if (string.IsNullOrEmpty(cname))
+                return Task.FromResult<DnsDatagram>(null);
+
+            IReadOnlyList<DnsResourceRecord> answers;
+
+            if (request.Question[0].Name.Equals(zoneName, StringComparison.OrdinalIgnoreCase)) //check for zone apex
+                answers = new DnsResourceRecord[] { new DnsResourceRecord(request.Question[0].Name, DnsResourceRecordType.ANAME, DnsClass.IN, appRecordTtl, new DnsANAMERecord(cname)) }; //use ANAME
+            else
+                answers = new DnsResourceRecord[] { new DnsResourceRecord(request.Question[0].Name, DnsResourceRecordType.CNAME, DnsClass.IN, appRecordTtl, new DnsCNAMERecord(cname)) };
+
+            return Task.FromResult(new DnsDatagram(request.Identifier, true, request.OPCODE, true, false, request.RecursionDesired, isRecursionAllowed, false, false, DnsResponseCode.NoError, request.Question, answers));
+        }
+
+        #endregion
+
+        #region properties
+
+        public string Description
+        { get { return "Returns different CNAME record for clients querying over public and private networks. Note that the app will return ANAME record for an APP record at zone apex."; } }
+
+        public string ApplicationRecordDataTemplate
+        {
+            get
+            {
+                return @"{
+  ""public"": ""api.example.com"",
+  ""private"": ""api.example.corp""
+}";
+            }
+        }
+
+        #endregion
+    }
+}
