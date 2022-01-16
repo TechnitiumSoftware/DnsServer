40d84b1120d21b3e8f7c7aeb38d29e9b3371f6e4
Shreyas Zare <shreyas@technitium.com>
2022-01-16 19:43:32 +0530

    ForwarderZone: using ApexZone.

--- DnsServerCore/Dns/Zones/ForwarderZone.cs
+++ DnsServerCore/Dns/Zones/ForwarderZone.cs
@@ -1,6 +1,6 @@
 ﻿/*
 Technitium DNS Server
-Copyright (C) 2021  Shreyas Zare (shreyas@technitium.com)
+Copyright (C) 2022  Shreyas Zare (shreyas@technitium.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@ using TechnitiumLibrary.Net.Dns.ResourceRecords;
 
 namespace DnsServerCore.Dns.Zones
 {
-    class ForwarderZone : AuthZone
+    class ForwarderZone : ApexZone
     {
         #region constructor
 
