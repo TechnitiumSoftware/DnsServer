b33b4a0c523504016f56be9b448662bb8d0ac387
Shreyas Zare <shreyas@technitium.com>
2022-02-19 12:58:17 +0530

    AllowedZoneManager: updated default SOA values.

--- DnsServerCore/Dns/ZoneManagers/AllowedZoneManager.cs
+++ DnsServerCore/Dns/ZoneManagers/AllowedZoneManager.cs
@@ -58,7 +58,7 @@ namespace DnsServerCore.Dns.ZoneManagers
 
         private void UpdateServerDomain(string serverDomain)
         {
-            _soaRecord = new DnsSOARecord(serverDomain, "hostadmin." + serverDomain, 1, 14400, 3600, 604800, 60);
+            _soaRecord = new DnsSOARecord(serverDomain, "hostadmin." + serverDomain, 1, 900, 300, 604800, 60);
             _nsRecord = new DnsNSRecord(serverDomain);
 
             _zoneManager.ServerDomain = serverDomain;
