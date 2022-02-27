33894261bfb521ebb5e9784407c88f4e0fd75c7e
Shreyas Zare <shreyas@technitium.com>
2022-02-27 17:52:37 +0530

    QueryLogsSqlite: minor changes.

--- Apps/QueryLogsSqliteApp/App.cs
+++ Apps/QueryLogsSqliteApp/App.cs
@@ -1,6 +1,6 @@
 ﻿/*
 Technitium DNS Server
-Copyright (C) 2021  Shreyas Zare (shreyas@technitium.com)
+Copyright (C) 2022  Shreyas Zare (shreyas@technitium.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -422,7 +422,7 @@ CREATE TABLE IF NOT EXISTS dns_logs
 
             if (qname is not null)
             {
-                if (qname.Contains("*"))
+                if (qname.Contains('*'))
                 {
                     whereClause += "qname like @qname AND ";
                     qname = qname.Replace("*", "%");
