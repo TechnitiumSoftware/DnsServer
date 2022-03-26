7eb773b0eb841b40e7fee40b49cbc9d685a8f7cc
Shreyas Zare <shreyas@technitium.com>
2022-03-26 11:42:23 +0530

    AllowedZoneManager: implemented Flush().

--- DnsServerCore/Dns/ZoneManagers/AllowedZoneManager.cs
+++ DnsServerCore/Dns/ZoneManagers/AllowedZoneManager.cs
@@ -132,6 +132,11 @@ namespace DnsServerCore.Dns.ZoneManagers
             return false;
         }
 
+        public void Flush()
+        {
+            _zoneManager.Flush();
+        }
+
         public List<AuthZoneInfo> ListZones()
         {
             return _zoneManager.ListZones();
