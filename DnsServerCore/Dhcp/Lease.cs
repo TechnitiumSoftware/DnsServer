c52211fb47f1ef3e660aa10d9a1fd74d084c7d38
Shreyas Zare <shreyas@technitium.com>
2022-04-30 12:02:17 +0530

    Lease: minor refactoring done.

--- DnsServerCore/Dhcp/Lease.cs
+++ DnsServerCore/Dhcp/Lease.cs
@@ -1,6 +1,6 @@
 ﻿/*
 Technitium DNS Server
-Copyright (C) 2021  Shreyas Zare (shreyas@technitium.com)
+Copyright (C) 2022  Shreyas Zare (shreyas@technitium.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -169,7 +169,7 @@ namespace DnsServerCore.Dhcp
             bW.Write(_leaseExpires);
         }
 
-        public string GetClientFullIdentifier()
+        public string GetClientInfo()
         {
             string hardwareAddress = BitConverter.ToString(_hardwareAddress);
 
