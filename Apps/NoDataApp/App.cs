86853e0e18991c81875a32be990403cf4bb05974
Shreyas Zare <shreyas@technitium.com>
2022-11-12 15:38:46 +0530

    NoDataApp: implemented no data app to be used to block DNS resource records in forwarder zones with a NO DATA response.

---
+++ Apps/NoDataApp/App.cs
@@ -0,0 +1,91 @@
+﻿/*
+Technitium DNS Server
+Copyright (C) 2022  Shreyas Zare (shreyas@technitium.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+using DnsServerCore.ApplicationCommon;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using TechnitiumLibrary.Net.Dns;
+using TechnitiumLibrary.Net.Dns.ResourceRecords;
+
+namespace NoData
+{
+    public class App : IDnsApplication, IDnsAppRecordRequestHandler
+    {
+        #region IDisposable
+
+        public void Dispose()
+        {
+            //do nothing
+        }
+
+        #endregion
+
+        #region public
+
+        public Task InitializeAsync(IDnsServer dnsServer, string config)
+        {
+            //do nothing
+            return Task.CompletedTask;
+        }
+
+        public Task<DnsDatagram> ProcessRequestAsync(DnsDatagram request, IPEndPoint remoteEP, DnsTransportProtocol protocol, bool isRecursionAllowed, string zoneName, string appRecordName, uint appRecordTtl, string appRecordData)
+        {
+            DnsQuestionRecord question = request.Question[0];
+
+            if (question.Name.Equals(appRecordName, StringComparison.OrdinalIgnoreCase))
+            {
+                dynamic jsonAppRecordData = JsonConvert.DeserializeObject(appRecordData);
+
+                foreach (dynamic jsonBlockedType in jsonAppRecordData.blockedTypes)
+                {
+                    DnsResourceRecordType blockedType = Enum.Parse<DnsResourceRecordType>(jsonBlockedType.Value, true);
+                    if ((blockedType == question.Type) || (blockedType == DnsResourceRecordType.ANY))
+                        return Task.FromResult(new DnsDatagram(request.Identifier, true, request.OPCODE, false, false, request.RecursionDesired, isRecursionAllowed, false, false, DnsResponseCode.NoError, request.Question));
+                }
+            }
+
+            return Task.FromResult<DnsDatagram>(null);
+        }
+
+        #endregion
+
+        #region properties
+
+        public string Description
+        { get { return "Returns a NO DATA response for requests that query for the blocked resource record types in Conditional Forwarder zones."; } }
+
+        public string ApplicationRecordDataTemplate
+        {
+            get
+            {
+                return @"{
+  ""blockedTypes"": [
+    ""A"", 
+    ""AAAA"",
+    ""ANY""
+  ]
+}";
+            }
+        }
+
+        #endregion
+    }
+}
