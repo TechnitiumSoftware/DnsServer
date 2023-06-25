e10c856dce069e058062cb0cc3ef8c7d1e1e3165
Shreyas Zare <shreyas@technitium.com>
2023-06-25 19:14:41 +0530

    DnsWebService: added new zone convert api support.

--- DnsServerCore/DnsWebService.cs
+++ DnsServerCore/DnsWebService.cs
@@ -359,6 +359,7 @@ namespace DnsServerCore
             //zones
             _webService.MapGetAndPost("/api/zones/list", _zonesApi.ListZones);
             _webService.MapGetAndPost("/api/zones/create", _zonesApi.CreateZoneAsync);
+            _webService.MapGetAndPost("/api/zones/convert", _zonesApi.ConvertZone);
             _webService.MapGetAndPost("/api/zones/enable", _zonesApi.EnableZone);
             _webService.MapGetAndPost("/api/zones/disable", _zonesApi.DisableZone);
             _webService.MapGetAndPost("/api/zones/delete", _zonesApi.DeleteZone);
