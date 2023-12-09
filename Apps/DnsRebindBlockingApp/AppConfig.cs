fe00b00e6d1fd323d003b36b4d7f23444b821ddb
Yip Rui Fung <rf@yrf.me>
2023-12-09 13:02:47 +0800

    Add DNS Rebind Protection as an DNSApp.

---
+++ Apps/DnsRebindBlockingApp/AppConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DnsRebindBlocking;
+
+public class AppConfig
+{
+    public required bool Enabled { get; set; }
+    public required List<string> PrivateNetworks { get; init; } = new();
+    public required HashSet<string> PrivateDomains { get; init; } = new();
+}
