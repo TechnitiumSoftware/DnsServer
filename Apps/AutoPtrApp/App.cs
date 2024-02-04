b8578012ff047322af11a9b351a4fa81922d3586
Shreyas Zare <shreyas@technitium.com>
2024-02-04 16:56:04 +0530

    AutoPtr: code refactoring changes done.

--- Apps/AutoPtrApp/App.cs
+++ Apps/AutoPtrApp/App.cs
@@ -1,6 +1,6 @@
 ﻿/*
 Technitium DNS Server
-Copyright (C) 2023  Shreyas Zare (shreyas@technitium.com)
+Copyright (C) 2024  Shreyas Zare (shreyas@technitium.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -28,7 +28,7 @@ using TechnitiumLibrary.Net.Dns.ResourceRecords;
 
 namespace AutoPtr
 {
-    public class App : IDnsApplication, IDnsAppRecordRequestHandler
+    public sealed class App : IDnsApplication, IDnsAppRecordRequestHandler
     {
         #region variables
 
