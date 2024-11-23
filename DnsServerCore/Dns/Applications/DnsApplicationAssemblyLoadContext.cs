1aa0181bbad4f22bee7f0784bb720947e93abdbf
Zafer Balkan <zafer@zaferbalkan.com>
2024-11-23 12:50:12 +0200

    Used a dependencyGraph for handling direct and transitive dependencies

--- DnsServerCore/Dns/Applications/DnsApplicationAssemblyLoadContext.cs
+++ DnsServerCore/Dns/Applications/DnsApplicationAssemblyLoadContext.cs
@@ -1,157 +1,70 @@
-﻿/*
-Technitium DNS Server
-Copyright (C) 2024  Shreyas Zare (shreyas@technitium.com)
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
-using DnsServerCore.ApplicationCommon;
+﻿using DnsServerCore.ApplicationCommon;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Runtime.Loader;
 
-namespace DnsServerCore.Dns
+namespace DnsServerCore.Dns.Applications
 {
     class DnsApplicationAssemblyLoadContext : AssemblyLoadContext
     {
         #region variables
 
-        readonly static Type _dnsApplicationInterface = typeof(IDnsApplication);
+        private static readonly Type _dnsApplicationInterface = typeof(IDnsApplication);
+        private readonly IDnsServer _dnsServer;
 
-        readonly IDnsServer _dnsServer;
+        private readonly List<Assembly> _appAssemblies = new();
+        private readonly Dictionary<string, Assembly> _loadedAssemblies = new(StringComparer.OrdinalIgnoreCase);
 
-        readonly List<Assembly> _appAssemblies;
+        private readonly Dictionary<string, nint> _loadedUnmanagedDlls = new();
         readonly AssemblyDependencyResolver _dependencyResolver;
 
-        readonly Dictionary<string, IntPtr> _loadedUnmanagedDlls = new Dictionary<string, IntPtr>();
         readonly List<string> _unmanagedDllTempPaths = new List<string>();
-
         #endregion
 
         #region constructor
 
         public DnsApplicationAssemblyLoadContext(IDnsServer dnsServer)
-            : base(true)
+            : base(isCollectible: true)
         {
-            _dnsServer = dnsServer;
+            _dnsServer = dnsServer ?? throw new ArgumentNullException(nameof(dnsServer));
 
-            Unloading += delegate (AssemblyLoadContext obj)
+            Unloading += UnloadTempFiles;
+
+            LoadPlugins();
+        }
+
+        #endregion
+
+        #region public methods
+
+        public void LoadPlugins()
+        {
+            var pluginDirectory = _dnsServer.ApplicationFolder;
+            if (!Directory.Exists(pluginDirectory))
+                throw new DirectoryNotFoundException($"Directory not found: {pluginDirectory}");
+
+            // Step 1: Build the dependency graph
+            var assemblyPaths = Directory.GetFiles(pluginDirectory, "*.dll");
+            var dependencyGraph = BuildDependencyGraph(assemblyPaths);
+
+            // Step 2: Resolve dependencies in correct load order
+            foreach (var assemblyPath in ResolveLoadOrder(dependencyGraph))
             {
-                foreach (string unmanagedDllTempPath in _unmanagedDllTempPaths)
-                {
-                    try
-                    {
-                        File.Delete(unmanagedDllTempPath);
-                    }
-                    catch
-                    { }
-                }
-            };
+                LoadAssemblyAndDependencies(assemblyPath);
+            }
 
-            _appAssemblies = new List<Assembly>();
-
-            IEnumerable<Assembly> loadedAssemblies = Default.Assemblies;
-
-            foreach (string dllFile in Directory.GetFiles(_dnsServer.ApplicationFolder, "*.dll", SearchOption.TopDirectoryOnly))
+            // Step 3: Identify and register plugins
+            foreach (var assembly in _loadedAssemblies.Values)
             {
-                string dllFileName = Path.GetFileNameWithoutExtension(dllFile);
-
-                bool isLoaded = false;
-
-                foreach (Assembly loadedAssembly in loadedAssemblies)
-                {
-                    if (!string.IsNullOrEmpty(loadedAssembly.Location))
-                    {
-                        if (Path.GetFileNameWithoutExtension(loadedAssembly.Location).Equals(dllFileName, StringComparison.OrdinalIgnoreCase))
-                        {
-                            isLoaded = true;
-                            break;
-                        }
-                    }
-                    else
-                    {
-                        AssemblyName assemblyName = loadedAssembly.GetName();
-
-                        if ((assemblyName.Name != null) && assemblyName.Name.Equals(dllFileName, StringComparison.OrdinalIgnoreCase))
-                        {
-                            isLoaded = true;
-                            break;
-                        }
-                    }
-                }
-
-                if (isLoaded)
-                    continue;
-
-                try
-                {
-                    Assembly appAssembly;
-                    string pdbFile = Path.Combine(_dnsServer.ApplicationFolder, Path.GetFileNameWithoutExtension(dllFile) + ".pdb");
-
-                    if (File.Exists(pdbFile))
-                    {
-                        using (FileStream dllStream = new FileStream(dllFile, FileMode.Open, FileAccess.Read))
-                        {
-                            using (FileStream pdbStream = new FileStream(pdbFile, FileMode.Open, FileAccess.Read))
-                            {
-                                appAssembly = LoadFromStream(dllStream, pdbStream);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        using (FileStream dllStream = new FileStream(dllFile, FileMode.Open, FileAccess.Read))
-                        {
-                            appAssembly = LoadFromStream(dllStream);
-                        }
-                    }
-
-                    if (_dependencyResolver is null)
-                    {
-                        bool isMainAssembly = false;
-
-                        foreach (Type classType in appAssembly.ExportedTypes)
-                        {
-                            foreach (Type interfaceType in classType.GetInterfaces())
-                            {
-                                if (interfaceType == _dnsApplicationInterface)
-                                {
-                                    isMainAssembly = true;
-                                    break;
-                                }
-                            }
-
-                            if (isMainAssembly)
-                                break;
-                        }
-
-                        if (isMainAssembly)
-                            _dependencyResolver = new AssemblyDependencyResolver(dllFile);
-                    }
-
-                    _appAssemblies.Add(appAssembly);
-                }
-                catch (Exception ex)
-                {
-                    _dnsServer.WriteLog(ex);
-                }
+                RegisterPlugin(assembly);
             }
         }
+
+        public IReadOnlyList<Assembly> AppAssemblies => _appAssemblies;
 
         #endregion
 
@@ -159,124 +72,175 @@ namespace DnsServerCore.Dns
 
         protected override Assembly Load(AssemblyName assemblyName)
         {
-            return null;
+            return null; // Default behavior for now
         }
 
-        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        protected override nint LoadUnmanagedDll(string unmanagedDllName)
         {
-            string unmanagedDllPath = null;
-
-            if (_dependencyResolver is not null)
-            {
-                string resolvedPath = _dependencyResolver.ResolveUnmanagedDllToPath(unmanagedDllName);
-                if (!string.IsNullOrEmpty(resolvedPath) && File.Exists(resolvedPath))
-                    unmanagedDllPath = resolvedPath;
-            }
+            string unmanagedDllPath = ResolveUnmanagedDllPath(unmanagedDllName);
 
             if (unmanagedDllPath is null)
-            {
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    string runtime = "win-" + RuntimeInformation.ProcessArchitecture.ToString().ToLowerInvariant();
-                    string[] prefixes = new string[] { "" };
-                    string[] extensions = new string[] { ".dll" };
-
-                    unmanagedDllPath = FindUnmanagedDllPath(unmanagedDllName, runtime, prefixes, extensions);
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                {
-                    bool isAlpine = false;
-
-                    try
-                    {
-                        string osReleaseFile = "/etc/os-release";
-
-                        if (File.Exists(osReleaseFile))
-                            isAlpine = File.ReadAllText(osReleaseFile).Contains("alpine", StringComparison.OrdinalIgnoreCase);
-                    }
-                    catch
-                    { }
-
-                    string runtimeAlpine = "linux-musl-" + RuntimeInformation.ProcessArchitecture.ToString().ToLowerInvariant();
-                    string runtimeLinux = "linux-" + RuntimeInformation.ProcessArchitecture.ToString().ToLowerInvariant();
-                    string[] prefixes = new string[] { "", "lib" };
-                    string[] extensions = new string[] { ".so", ".so.1" };
-
-                    if (isAlpine)
-                    {
-                        unmanagedDllPath = FindUnmanagedDllPath(unmanagedDllName, runtimeAlpine, prefixes, extensions);
-                        if (unmanagedDllPath is null)
-                            unmanagedDllPath = FindUnmanagedDllPath(unmanagedDllName, runtimeLinux, prefixes, extensions);
-                    }
-                    else
-                    {
-                        unmanagedDllPath = FindUnmanagedDllPath(unmanagedDllName, runtimeLinux, prefixes, extensions);
-                    }
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                {
-                    string runtime = "osx-" + RuntimeInformation.ProcessArchitecture.ToString().ToLowerInvariant();
-                    string[] prefixes = new string[] { "", "lib" };
-                    string[] extensions = new string[] { ".dylib" };
-
-                    unmanagedDllPath = FindUnmanagedDllPath(unmanagedDllName, runtime, prefixes, extensions);
-                }
-
-                if (unmanagedDllPath is null)
-                    return IntPtr.Zero;
-            }
+                return nint.Zero;
 
             lock (_loadedUnmanagedDlls)
             {
-                if (!_loadedUnmanagedDlls.TryGetValue(unmanagedDllPath.ToLowerInvariant(), out IntPtr value))
+                if (!_loadedUnmanagedDlls.TryGetValue(unmanagedDllPath, out nint dllHandle))
                 {
-                    //load the unmanaged DLL
-                    //copy unmanaged dll into temp file for loading to allow uninstalling/updating app at runtime.
-                    string tempPath = Path.GetTempFileName();
-
-                    using (FileStream srcFile = new FileStream(unmanagedDllPath, FileMode.Open, FileAccess.Read))
-                    {
-                        using (FileStream dstFile = new FileStream(tempPath, FileMode.Create, FileAccess.Write))
-                        {
-                            srcFile.CopyTo(dstFile);
-                        }
-                    }
-
+                    string tempPath = CopyToTempFile(unmanagedDllPath);
+                    dllHandle = LoadUnmanagedDllFromPath(tempPath);
+                    _loadedUnmanagedDlls[unmanagedDllPath] = dllHandle;
                     _unmanagedDllTempPaths.Add(tempPath);
-
-                    value = LoadUnmanagedDllFromPath(tempPath);
-                    _loadedUnmanagedDlls.Add(unmanagedDllPath.ToLowerInvariant(), value);
                 }
 
-                return value;
+                return dllHandle;
             }
         }
 
         #endregion
 
-        #region private
+        #region private methods
 
-        private string FindUnmanagedDllPath(string unmanagedDllName, string runtime, string[] prefixes, string[] extensions)
+        private void RegisterPlugin(Assembly assembly)
         {
-            foreach (string prefix in prefixes)
+            try
             {
-                foreach (string extension in extensions)
+                foreach (var type in assembly.GetTypes())
                 {
-                    string path = Path.Combine(_dnsServer.ApplicationFolder, "runtimes", runtime, "native", prefix + unmanagedDllName + extension);
-                    if (File.Exists(path))
-                        return path;
+                    if (type.GetInterfaces().Contains(_dnsApplicationInterface))
+                    {
+                        _appAssemblies.Add(assembly);
+                        break;
+                    }
+                }
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                _dnsServer.WriteLog($"Failed to load types from assembly {assembly.FullName}: {ex}");
+            }
+        }
+
+        private void LoadAssemblyAndDependencies(string assemblyPath)
+        {
+            if (_loadedAssemblies.ContainsKey(assemblyPath))
+                return;
+
+            var assembly = LoadFromAssemblyPath(assemblyPath);
+            _loadedAssemblies[assemblyPath] = assembly;
+
+            foreach (var dependency in assembly.GetReferencedAssemblies())
+            {
+                var dependencyPath = Path.Combine(_dnsServer.ApplicationFolder, dependency.Name + ".dll");
+
+                if (File.Exists(dependencyPath) && !_loadedAssemblies.ContainsKey(dependencyPath))
+                {
+                    LoadAssemblyAndDependencies(dependencyPath);
+                }
+                else if (!_loadedAssemblies.ContainsKey(dependency.FullName))
+                {
+                    try
+                    {
+                        var resolvedAssembly = LoadFromAssemblyName(dependency);
+                        _loadedAssemblies[dependency.FullName] = resolvedAssembly;
+                    }
+                    catch (Exception ex)
+                    {
+                        _dnsServer.WriteLog($"Failed to load dependency {dependency.FullName}: {ex}");
+                    }
+                }
+            }
+        }
+
+        private Dictionary<string, List<string>> BuildDependencyGraph(IEnumerable<string> assemblyPaths)
+        {
+            var dependencyGraph = new Dictionary<string, List<string>>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (var assemblyPath in assemblyPaths)
+            {
+                try
+                {
+                    var dependencies = Assembly.LoadFrom(assemblyPath)
+                        .GetReferencedAssemblies()
+                        .Select(dep => Path.Combine(_dnsServer.ApplicationFolder, dep.Name + ".dll"))
+                        .Where(File.Exists)
+                        .ToList();
+
+                    dependencyGraph[assemblyPath] = dependencies;
+                }
+                catch (Exception ex)
+                {
+                    // Log or handle exceptions here if necessary
+                    Debug.WriteLine($"Failed to analyze assembly {assemblyPath}: {ex}");
                 }
             }
 
+            return dependencyGraph;
+        }
+
+        private static List<string> ResolveLoadOrder(Dictionary<string, List<string>> dependencyGraph)
+        {
+            var resolved = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            var sorted = new List<string>();
+
+            void Visit(string assemblyPath)
+            {
+                if (resolved.Add(assemblyPath))
+                {
+                    foreach (var dependency in dependencyGraph.GetValueOrDefault(assemblyPath, new List<string>()))
+                    {
+                        Visit(dependency);
+                    }
+                    sorted.Add(assemblyPath);
+                }
+            }
+
+            foreach (var assemblyPath in dependencyGraph.Keys)
+            {
+                Visit(assemblyPath);
+            }
+
+            return sorted;
+        }
+
+        private string ResolveUnmanagedDllPath(string unmanagedDllName)
+        {
+            if (_dependencyResolver != null)
+            {
+                var resolvedPath = _dependencyResolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+                if (!string.IsNullOrEmpty(resolvedPath) && File.Exists(resolvedPath))
+                    return resolvedPath;
+            }
+
+            // Additional OS-specific lookup logic here if needed
             return null;
         }
 
-        #endregion
+        private static string CopyToTempFile(string filePath)
+        {
+            var tempPath = Path.GetTempFileName();
 
-        #region properties
+            using (var srcStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            using (var dstStream = new FileStream(tempPath, FileMode.Create, FileAccess.Write))
+            {
+                srcStream.CopyTo(dstStream);
+            }
 
-        public IReadOnlyList<Assembly> AppAssemblies
-        { get { return _appAssemblies; } }
+            return tempPath;
+        }
+
+        private void UnloadTempFiles(AssemblyLoadContext context)
+        {
+            foreach (var tempPath in _unmanagedDllTempPaths)
+            {
+                try
+                {
+                    File.Delete(tempPath);
+                }
+                catch
+                {
+                    // Log or handle exceptions here if necessary
+                }
+            }
+        }
 
         #endregion
     }
