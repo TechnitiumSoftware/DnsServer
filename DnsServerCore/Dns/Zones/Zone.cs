89181aae82f95f48e911c6c64c282c0460483db3
Shreyas Zare <shreyas@technitium.com>
2025-04-19 15:48:25 +0530

    minor change

--- DnsServerCore/Dns/Zones/Zone.cs
+++ DnsServerCore/Dns/Zones/Zone.cs
@@ -1,6 +1,6 @@
 ﻿/*
 Technitium DNS Server
-Copyright (C) 2024  Shreyas Zare (shreyas@technitium.com)
+Copyright (C) 2025  Shreyas Zare (shreyas@technitium.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
